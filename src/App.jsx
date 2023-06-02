f8f50caf32f1a2782ded179c046e4f6eb97a221e
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-06-02 09:39:21 +0100

    first axios api build

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { useState, useEffect } from 'react'
+import Axios from 'axios'
 
-function App() {
+const App = () => {
+
+  const [catFact,setCatFact] = useState('')
+
+  const fetchCatFact =()=>{
+    Axios.get('https://catfact.ninja/fact').then((res)=>setCatFact(res.data))
+  
+  }
+
+  useEffect(()=>{
+  },[])
+  
 
   return (
-    <div></div>
+    <div className='mx-auto space-y-4 mt-10 flex justify-center items-center flex-col'>
+      <button className='py-2 px-4 bg-gray-700 text-white' onClick={fetchCatFact}>Get API Data</button>
+      <div className='text-black p-8 w-96 h-48 border-2 border-black rounded-md'>{catFact.fact}</div>
+    </div>
   )
 }
 
