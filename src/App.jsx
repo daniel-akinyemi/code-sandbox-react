7c90385f29ddaf7d6a78a4e72abd286df50716b4
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-06-02 10:08:52 +0100

    predict age web app

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,25 @@ import Axios from 'axios'
 
 const App = () => {
 
-  const [catFact,setCatFact] = useState('')
+  const [name,setName] = useState('')
+  const [result,setResult] = useState(null)
 
-  const fetchCatFact =()=>{
-    Axios.get('https://catfact.ninja/fact').then((res)=>setCatFact(res.data))
+  const fetchData =()=>{
+    
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res)=>setResult(res.data))
   
   }
 
   useEffect(()=>{
+    fetchData()
   },[])
   
 
   return (
     <div className='mx-auto space-y-4 mt-10 flex justify-center items-center flex-col'>
-      <button className='py-2 px-4 bg-gray-700 text-white' onClick={fetchCatFact}>Get API Data</button>
-      <div className='text-black p-8 w-96 h-48 border-2 border-black rounded-md'>{catFact.fact}</div>
+      <input placeholder='Pedro' className='border-2 rounded-md px-4' onChange={(e)=> {setName(e.target.value)}}/>
+      <button onClick={fetchData} className='bg-gray-700 rounded-md px-4 py-2 text-white'>Predict Age</button>
+      <h1 className='my-8 text-4xl font-bold'>{result?.age}</h1>
     </div>
   )
 }
