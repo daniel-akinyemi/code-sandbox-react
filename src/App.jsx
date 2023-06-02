90becdbc9dca6224feb50e6b285c0fae2dec6ec4
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-06-02 10:44:03 +0100

    excuse web app (refetching issue)

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,28 @@ import Axios from 'axios'
 
 const App = () => {
 
-  const [name,setName] = useState('')
   const [result,setResult] = useState(null)
 
-  const fetchData =()=>{
+  const fetchData =(name)=>{
     
-    Axios.get(`https://api.agify.io/?name=${name}`).then((res)=>setResult(res.data))
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${name}`).then((res)=>setResult(res.data))
   
   }
 
-  useEffect(()=>{
-    fetchData()
-  },[])
+useEffect(() => {
+  fetchData()
+},[])
   
 
   return (
-    <div className='mx-auto space-y-4 mt-10 flex justify-center items-center flex-col'>
-      <input placeholder='Pedro' className='border-2 rounded-md px-4' onChange={(e)=> {setName(e.target.value)}}/>
-      <button onClick={fetchData} className='bg-gray-700 rounded-md px-4 py-2 text-white'>Predict Age</button>
-      <h1 className='my-8 text-4xl font-bold'>{result?.age}</h1>
+    <div className='mx-auto w-3/4 space-y-4 mt-10 flex justify-center items-center flex-col'>
+      <button className='bg-gray-800 rounded-md text-white px-4 py-2' onClick={fetchData('party')}>Party</button>
+
+      <button className='bg-gray-800 rounded-md text-white px-4 py-2' onClick={fetchData('family')}>Family</button>
+
+      <button className='bg-gray-800 rounded-md text-white px-4 py-2' onClick={fetchData('office')}>Office</button>
+
+      <div className='text-4xl font-bold my-8'>{result[0]?.excuse}</div>
     </div>
   )
 }
