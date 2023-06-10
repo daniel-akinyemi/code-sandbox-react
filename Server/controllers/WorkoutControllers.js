0346de09e6958bcfad46a86ce8309d699e233258
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-06-10 09:44:57 +0100

    error for get single workout fixed

--- Server/controllers/WorkoutControllers.js
+++ Server/controllers/WorkoutControllers.js
@@ -1,9 +1,37 @@
-import Workouts from "../models/Workouts";
+import Workout from "../models/Workouts.js";
+
+
+const getWorkouts = async (req,res)=>{ 
+    try{
+        const workouts = await Workout.find({}).sort({createdAt: -1})
+    res.status(200).json(workouts)
+    }catch(err){
+        res.status(400).json({error: err.message})
+    }
+}
+
+
+const getWorkout = async (req,res) => {
+    try{ 
+        const {id} = req.params
+
+    const workout = await Workout.findById(id)
+    if (!workout){
+        return res.status(404).json({Error: "This workout cannot be found"})
+    }
+    res.status(200).json(workout)
+
+    }catch(err){
+        console.log({error: err.message})
+    }
+    
+}
+
 
 const createWorkout = async (req,res)=>{
     const{title,load,reps} = req.body
     try{
-        const workout = await Workouts.create({title,load,reps})
+        const workout  = await Workout.create({title,load,reps})
         res.status(200).json(workout)
     }
     catch(err){
